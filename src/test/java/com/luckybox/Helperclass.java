28407b8f23ec5d18888c6ca7d4e7103d852b8b3c
jatinpandey321 <jatinwebsultanate@gmail.com>
2025-05-22 18:58:16 +0530

    commit browser open

--- src/test/java/com/luckybox/Helperclass.java
+++ src/test/java/com/luckybox/Helperclass.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Helperclass {
 
@@ -19,7 +20,12 @@ public class Helperclass {
     public static WebDriver getDriver() {
         if (driver == null) {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\websu\\Desktop\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-            driver = new ChromeDriver();
+            
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             driver.get("http://54.205.123.182/");
         }
